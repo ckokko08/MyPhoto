d1abf8e1590de5cd45c54022d121f8db4c0b74db
Chihiro Kubota <rioneko4423@gmail.com>
2016-11-22 19:18:31 +0900

    Initial Commit

---
+++ MyPhoto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyPhoto
+//
+//  Created by 久保田千尋 on 2016/11/22.
+//  Copyright © 2016年 Chihiro Kubota. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
